2f6a4fcd72603c4f047a3ec8eb56085bea8dab70
root <root@shooopping.ca>
2018-04-13 11:06:24 -0400

    fixed basic auth logic

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -14,14 +14,14 @@ class BasicAuthMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    
+
     public function handle($request, Closure $next) {
-        
+
         $email = $request->header('PHP_AUTH_USER');
         $pass = $request->header('PHP_AUTH_PW');
-        $customerID = $this->validate($email, $pass);
+        $customerID = $this->getCustomerId($email, $pass);
 
-        if(!isset($email, $pass) AND $customerID) {
+        if(!isset($email, $pass) OR !$customerID) {
             $headers = array('WWW-Authenticate' => 'Basic');
             return response('unauthorized', 401, $headers);
         }
@@ -30,10 +30,10 @@ class BasicAuthMiddleware
         return $next($request);
     }
 
-    private function validate($email, $pass){
+    private function getCustomerId($email, $pass){
         // Query taken from opencart login customer
         $customer = collect(DB::select("SELECT customer_id FROM oc_customer WHERE LOWER(email) = :email AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1(:pass))))) OR password = :md5pass) AND status = '1' AND approved = '1'", ['email' => $email, 'pass' => $pass, 'md5pass' => md5($pass)]))->first();
-        
+
         $customerID = empty($customer->customer_id) ? 0 : $customer->customer_id;
         return $customerID;
     }
